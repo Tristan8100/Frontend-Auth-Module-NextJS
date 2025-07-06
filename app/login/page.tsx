e4fa20fc323220ccfe4cbc64955fa087882254d2
tristan2603 <gtristan543@gmail.com>
2025-07-06 20:03:13 +0800

    added login UI

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,61 +1,23 @@
 'use client';
 
-import { useState } from 'react';
-import { useMutation } from '@tanstack/react-query';
-import { api } from '@/lib/api'; // Your existing Axios instance
-import { useRouter } from 'next/navigation';
-import { Router } from 'lucide-react';
-import { useAuth } from '@/contexts/AuthContext';
+import { LoginForm } from "@/components/login-form";
+import LoginPageTrial from "@/components/trial/login";
+import { GalleryVerticalEnd } from "lucide-react"
 
 export default function LoginPage() {
-  const {login } = useAuth();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const router = useRouter();
-
-  const loginMutation = useMutation({
-    mutationFn: (credentials: { email: string; password: string }) => 
-      api.post('/api/login', credentials).then(res => res.data),
-    onSuccess: (data) => {
-      if (data.token) {
-        console.log('Login successful:', data);
-        login(data.user_info, data.token); // Call login from AuthContext
-        router.push('/dashboard'); // Redirect to dashboard on success
-      }
-    },
-  });
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    loginMutation.mutate({ email, password });
-  };
+  
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          required
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          required
-        />
-        <button type="submit" disabled={loginMutation.isPending}>
-          {loginMutation.isPending ? 'Logging in...' : 'Login'}
-        </button>
-      </form>
-      
-      {loginMutation.isError && (
-        <div>Error: {loginMutation.error.message}</div>
-      )}
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <GalleryVerticalEnd className="size-4" />
+          </div>
+          Acme Inc.
+        </a>
+        <LoginForm />
+      </div>
     </div>
   );
 }
